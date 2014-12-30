bc48379b04575c9327e081e6b7832eda6a5147c8
hrb1658 <bergerharry@rocketmail.com>
2014-12-30 17:54:15 -0500

    Fixed src

--- src/main/java/com/hrb1658/coalcompressor/Strings.java
+++
@@ -1,9 +0,0 @@
-package com.hrb1658.coalcompressor;
-
-public class Strings {
-	
-	public static final String MOD_ID = "coalcompressor";
-	public static final String MOD_NAME = "Coal Compressor Mod";
-	public static final String MOD_VERSION = "1.1";
-	
-}
